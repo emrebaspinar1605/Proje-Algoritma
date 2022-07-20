706029a8ba779e07e53802f4bb17b6d0fd9cbaa0
emrebaspinar1605 <turkuz-elhamdulillah@hotmail.com>
2022-07-20 16:54:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            int index;
+            Console.WriteLine("Kelime Giriniz");
+            word = Console.ReadLine();
+            Console.WriteLine("Silmek istediğiniz harfi giriniz");
+            index = int.Parse(Console.ReadLine());
+
+            DeleteLetter(word,index);
+        }
+        static void DeleteLetter(string text,int x)
+        {
+            char[] letterArray = text.ToCharArray();
+            letterArray = letterArray.Where((source,index)=>index != x).ToArray();
+            Console.WriteLine(letterArray);
+        }
+        
+    }
+}
